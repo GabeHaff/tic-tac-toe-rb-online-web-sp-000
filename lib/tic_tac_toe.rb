54d51b02f5b7437c7a0ca55047ebab87e1432720
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-27 19:57:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -149,7 +149,7 @@ until over?(board)
  turn(board)
 end 
 if won?(board)
-  puts "Congratulations#{winner(board)}!"
+  puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
   puts "Cat's Game!"
 end
